c1b0e4e9d2dda6b1e43626c3c06f9c0003823c95
Alex Khab <finko85@gmail.com>
2019-10-25 02:18:08 +0300

    refactor code

--- src/js/main.js
+++ src/js/main.js
@@ -1,54 +1,52 @@
-"use strict";
+'use strict';
 
-import {isEscEvent, prepareFormData } from "./util";
-import { formInputHandler, formChangeHandler, checkPasswordField, form, formInputs, passwordField } from "./validation";
+import { doOnEscEvent, prepareFormData } from './util';
+import { fieldInputHandler, formChangeHandler, resetFieldsErrorClass, form, formInputs } from './validation';
 
-const regBtn = document.querySelector('.reg__btn');
-const popup = document.querySelector('.reg-form');
-const closeBtn = popup.querySelector('.form__close-btn');
-const submitBtn = popup.querySelector('.form__btn');
+(function () {
+  const regBtn = document.querySelector('.reg__btn');
+  const popup = document.querySelector('.reg-form');
+  const closeBtn = popup.querySelector('.form__close-btn');
+  const submitBtn = popup.querySelector('.form__btn');
 
+  const popupEscClickHandler = (evt) => doOnEscEvent(evt, closePopup);
 
-let popupEscClickHandler = function (evt) {
-  isEscEvent(evt, closePopup);
-};
+  const openPopup = () =>  {
+    popup.classList.add('modal-show');
+    document.addEventListener('keydown', popupEscClickHandler);
+  };
 
-let openPopup = function () {
-  popup.classList.add('modal-show');
-  document.addEventListener('keydown', popupEscClickHandler);
-};
+  const closePopup = () =>  {
+    if (popup.classList.contains('modal-show')) {
+      popup.classList.remove('modal-show');
+    }
+    document.removeEventListener('keydown', popupEscClickHandler);
+  };
 
-let closePopup = function () {
-  if (popup.classList.contains('modal-show')) {
-    popup.classList.remove('modal-show');
-  }
-  document.removeEventListener('keydown', popupEscClickHandler);
-};
+  const formSubmitHandler = (evt) => {
+    evt.preventDefault();
+    let data = new FormData(form);
+    console.log(prepareFormData(data));
+    closePopup();
+    regBtn.classList.add('reg__btn--disabled');
+  };
 
-let formSubmitHandler = function (evt) {
-  evt.preventDefault();
-  let data = new FormData(form);
-  console.log(prepareFormData(data));
-  closePopup();
-  regBtn.classList.add('reg__btn--disabled');
-};
-
-regBtn.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  openPopup();
-});
-
-closeBtn.addEventListener('click', function () {
-  form.reset();
-  formInputs.forEach(function (item) {
-    item.classList.remove('form__input--error');
-    item.nextElementSibling.textContent = '';
+  regBtn.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    openPopup();
   });
-  closePopup();
-});
 
-submitBtn.disabled = true;
-form.addEventListener('change', formChangeHandler);
-form.addEventListener('input', formInputHandler);
-passwordField.addEventListener('input', checkPasswordField);
-form.addEventListener('submit', formSubmitHandler);
+  closeBtn.addEventListener('click', () => {
+    form.reset();
+    resetFieldsErrorClass();
+    closePopup();
+  });
+
+  formInputs.forEach((item) => {
+    item.addEventListener('input', fieldInputHandler);
+  });
+  form.addEventListener('change', formChangeHandler);
+  form.addEventListener('submit', formSubmitHandler);
+
+  submitBtn.disabled = true;
+})();
